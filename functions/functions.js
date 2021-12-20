a5eeff14e1864a12a9456c4a09a814fcdd964b05
Anderson Ramos <52046214+AndersonRamos07@users.noreply.github.com>
2021-12-20 15:52:00 +0000

    Mais alguns ajustes

--- functions/functions.js
+++ functions/functions.js
@@ -3,7 +3,6 @@ const funcFiles = require('./fileFunctions');
 const getFunc = require('./getFunction');
 
 const cArquivo = './arquivo/_arosBD.json';
-//const cArquivo = './arquivo/arosBD.json';
 
 const construir = (pNome, pSobreNome, pCPF, pEnderecos) => {
   var modeloNovo = new modelo(pNome, pSobreNome, pCPF, pEnderecos);
@@ -17,12 +16,8 @@ const construir = (pNome, pSobreNome, pCPF, pEnderecos) => {
     const tCAtualizado = tCSemColchete + ',\n' + dados + '\n]';
     funcFiles.aditarArquivo(tCAtualizado, cArquivo);
   }
-  catch(erro){
-    console.log(erro.message);
-  }
-  finally{
-  console.log(modeloNovo.showThis());
-  }  
+  catch(erro){ console.log(erro.message); }
+  finally{ console.log(modeloNovo.showThis()); }  
   return modeloNovo;
 };
 
@@ -30,25 +25,9 @@ const saveContent = ()=>{
   return funcFiles.pegarConteudo(cArquivo);
 };
 
-const getContent = () =>{
-  return getFunc.getModel(cArquivo);
-}
-
-const getContentConvert = () =>{
-  return getFunc.convertArray(cArquivo);
-}
-
 const pegarOConteudo = () =>{
   var content = funcFiles.pegarConteudo(cArquivo); 
   var tudo = getFunc.convertArray(content);
 }
 
-module.exports = {getContentConvert, construir, saveContent, getContent, pegarOConteudo}
-
-/*
-const pushContent = (pConteudo)=>{
-  var paraConverter = saveContent();
-  paraConverter.push(pConteudo);
-  return console.log('Acho que foi... <pushContent>')
-}
-*/
+module.exports = { construir, saveContent, pegarOConteudo }
