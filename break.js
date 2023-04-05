24db119f0f209e23bded2667103885bdf6d53754
AndersonRamos07 <andersonramos07@live.com>
2023-04-05 16:13:00 -0400

    modify break.js

--- break.js
+++ break.js
@@ -1,7 +1,38 @@
-function changeVisibility ()
+//#region Trecho 003
+//https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
+
+const imgs = document.querySelector("#imgs");
+
+imgs.addEventListener("change", (event) => {
+  imgs.checked? imagesVisibility (0):
+    imagesVisibility (1);
+});
+//#endregion Trecho 003
+
+function imagesVisibility (pStatus)
 {
-    let status = document.querySelector("#menulateral");
-    console.log(status.visibility)
-    //menuSet.visibility = 
+    let images = document.querySelectorAll(".images");
+    images.forEach(image => image.style.opacity = pStatus)
 }
-changeVisibility()
+imagesVisibility()
+
+//#region Trecho 002
+//https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
+document.addEventListener(
+    "keydown",
+    (e) => {
+      if (e.key === "Enter") {
+        toggleFullScreen();
+      }
+    },
+    false
+  );
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+//#endregion Trecho 002
